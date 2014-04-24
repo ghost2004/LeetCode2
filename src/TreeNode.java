9ed87ed2a710036a6cee72d53bc07eca3edef9c7
Tiangang Liu <liutg@hotmail.com>
2014-04-23 23:27:42 -0700

    TreeNode implementation

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -17,26 +17,83 @@ Here's an example:
 The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
  */
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
-    
-    public void printNode(TreeNode node) {
+    private static String ele[];
+    private static int idx;
+
+    private void printNode(TreeNode node, StringBuffer buf) {
         if (node == null) {
-            System.out.print("# ");
+            buf.append("#,");
         } else {
-            System.out.print(node.val+" ");
-            printNode(node.left);
-            printNode(node.right);
+            buf.append(node.val+",");
+            printNode(node.left, buf);
+            printNode(node.right, buf);
         }
     }
     
-    public void print() {
-        printNode(this);
-        System.out.println("");
+    public String serialize() {
+        StringBuffer buf = new StringBuffer();
+        printNode(this, buf);
+        int idx = buf.length()-1;
+        return buf.toString().substring(0, idx);
+    }
+    
+
+    public static String replaceBlank(String str) {
+        String dest = "";
+        if (str!=null) {
+            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            dest = m.replaceAll("");
+        }
+        return dest;
+    }
+    
+    public static boolean isInteger(String str) {
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");  
+        return pattern.matcher(str).matches(); 
+    }
+    
+    public static TreeNode getFromArray() {
+        
+        if (idx >= ele.length)
+            return null;
+        TreeNode node = null;
+        if (isInteger(ele[idx])) {
+            node = new TreeNode(Integer.parseInt(ele[idx]));
+            idx++;
+            node.left = getFromArray();
+
+            idx++;
+            node.right = getFromArray();
+        } 
+        
+        return node;
+        
+    }
+    
+    public static TreeNode deserialize(String input) {
+        if (input == null || input.length() == 0)
+            return null;
+        String stream = replaceBlank(input);
+        ele = stream.split(",");
+        idx = 0;
+        return getFromArray();
+        
+        
+    }
+    
+    public static void main(String args[]) {
+        TreeNode t = TreeNode.deserialize("1,2,3,#,#,4,#,#,5");
+        System.out.println(t.serialize());
+        
     }
 }
     
