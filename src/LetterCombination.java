2738f5cbd07323e8b513b9aac23269922d89d381
Tiangang Liu <liutg@hotmail.com>
2014-05-16 22:00:35 -0700

    Letter combination

--- src/LetterCombination.java
+++ src/LetterCombination.java
@@ -13,9 +13,26 @@ import java.util.ArrayList;
 public class LetterCombination {
     public static final String trans[] = {"", " ", "abc", "def", "ghi", "jkl",  
             "mno", "pqrs", "tuv", "wxyz"};
-    
+    public void addLetter(String prefix, String d, ArrayList<String> container){
+        int idx = d.charAt(0) - '0';
+        for (int i = 0; i < trans[idx].length(); i++) {
+            String np = prefix+trans[idx].charAt(i);
+            if (d.length() == 1) {
+                container.add(np);
+            } else {
+                addLetter(np,d.substring(1),container);
+            }
+        }
+        
+        
+    }
     public ArrayList<String> letterCombinations(String digits) {
         ArrayList<String> out = new ArrayList<String>();
+        if (digits == null || digits.length() == 0) {
+            out.add("");
+            return out;
+        }
+        addLetter("", digits,out);
         return out;
     }
                                
