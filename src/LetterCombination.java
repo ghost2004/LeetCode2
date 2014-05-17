0ff8b9cfde65e0368f44a30eacfcb2c93348f02a
Tiangang Liu <liutg@hotmail.com>
2014-05-16 21:36:07 -0700

    initial for letter combination

---
+++ src/LetterCombination.java
@@ -0,0 +1,22 @@
+/*
+ * Given a digit string, return all possible letter combinations 
+ * that the number could represent.
+ * 
+ * Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+Although the above answer is in lexicographical order, your answer
+ could be in any order you want.
+ */
+
+import java.util.ArrayList;
+public class LetterCombination {
+    public static final String trans[] = {"", " ", "abc", "def", "ghi", "jkl",  
+            "mno", "pqrs", "tuv", "wxyz"};
+    
+    public ArrayList<String> letterCombinations(String digits) {
+        ArrayList<String> out = new ArrayList<String>();
+        return out;
+    }
+                               
+}
