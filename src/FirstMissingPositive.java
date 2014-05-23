a044012edeb8d21b2d64e713eb15ab64daaccc67
Tiangang Liu <liutg@hotmail.com>
2014-05-22 23:32:31 -0700

    First missing positive

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -11,8 +11,28 @@ public class FirstMissingPositive {
     public int firstMissingPositive(int[] A) {
         int length = A.length;
         
+        if (A == null || A.length == 0)
+            return 1;
         
-        return length;
+        int i = 0;
+        
+        while (i < length) {
+            if (A[i] > 0 && A[i] < A.length && A[i] != i+1 && A[A[i]-1]!=A[i])  {
+                int tmp = A[A[i]-1];
+                A[A[i]-1] = A[i];
+                A[i] = tmp;
+            }
+            else 
+                i++;
+        }
+        
+        for (i = 0; i < A.length;i++)
+        {
+            if (A[i] != i+1)
+                return i+1;
+        }
+        
+        return length+1;
         
     }
 }
