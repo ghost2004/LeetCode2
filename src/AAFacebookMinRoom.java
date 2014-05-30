ee1254713af2a800910005c38a7bf0b134600a28
Tiangang Liu <liutg@hotmail.com>
2014-05-29 23:33:35 -0700

    Minimal conference room for meetings initial

---
+++ src/AAFacebookMinRoom.java
@@ -0,0 +1,8 @@
+/*
+ * We have a list of scheduled meetings with begin and end time
+ * return the minimal numbers of conference room which can hold
+ * those meetings
+ */
+public class AAFacebookMinRoom {
+
+}
