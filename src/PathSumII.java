3cde378f2d3d159e0377226442c96228314af646
Tiangang Liu <liutg@hotmail.com>
2014-05-31 21:36:19 -0700

    Path sum II initial

---
+++ src/PathSumII.java
@@ -0,0 +1,28 @@
+/*
+ * Given a binary tree and a sum, find all root-to-leaf
+ *  paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+ */
+import java.util.*;
+public class PathSumII {
+    
+    public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+        ArrayList<ArrayList<Integer>> out = new ArrayList<ArrayList<Integer>>();
+        
+        return out;
+    }
+}
